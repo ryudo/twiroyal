4479388d6890d32bc7acb2f66e0616372da2b718
Ryudo <nkgw.ry@gmail.com>
2012-02-10 17:08:23 +0900

    ユーザー名とれてますかね

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
-    create!do |user|
+    create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["user_info"]["name"]
