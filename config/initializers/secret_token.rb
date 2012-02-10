105926e9130e924b9ebe9138a461055cef135cd8
Ryudo <nkgw.ry@gmail.com>
2012-02-10 15:36:27 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twiloyal::Application.config.secret_token = '2152373ef9a88fe6141a6b8152726d3324ca2cda665a3b481ed923ac0b1de5e35b6dd26a32efb087690faf2df932265243aaada92f9b3a0b4d1a998778c59db9'
